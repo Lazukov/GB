724b04ec0b6d1ea4a456ce8e719843530a93cf57
Lazukov <marrt@rambler.ru>
2021-01-21 16:05:59 +0300

    Старт

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebGB.Models;
+
+namespace WebGB.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public readonly List<Employeer> AllEmployees = new List<Employeer>() 
+        { 
+            new Employeer() { ID=1, Surname="Иванов", Name="Иван", Date=DateTime.Parse("10.01.1985"), Description="Описание Иванов"} , 
+            new Employeer() { ID =2, Surname = "Петров",Name="Егор",  Date=DateTime.Parse("10.01.1984"), Description="Описание Петров"}
+        };
+        public IActionResult Index()
+        {
+            return View("Default");
+        }
+
+        //Default/ Employeer
+        public IActionResult Employeers()
+        {
+            return View(AllEmployees);
+        }
+
+        //Default/DispEmployer
+        public IActionResult DispEmployer(int id)
+        {
+            return View(AllEmployees.Where(x=>x.ID==id).FirstOrDefault());
+        }
+    }
+}
---
+++ Models/Employeer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGB.Models
+{
+    public class Employeer
+    {
+        public int ID { get; set; }
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime  Date { get; set; }
+
+        public string Description { get; set; }
+    }
+}
