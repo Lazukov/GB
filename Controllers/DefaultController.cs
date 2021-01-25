6bc79200f88f967b2a1878316e4b9ae2e0a68665
Lazukov <marrt@rambler.ru>
2021-01-25 17:59:46 +0300

    Добавлены страницы и изменены ссылки MainMenu

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -30,5 +30,64 @@ namespace WebGB.Controllers
         {
             return View(AllEmployees.Where(x=>x.ID==id).FirstOrDefault());
         }
+
+        //Default/Shop
+        public IActionResult Shop()
+        {
+            return View("Shop");
+        }
+
+        //Default/Blog
+        public IActionResult Blog()
+        {
+            return View("Blog");
+        }
+
+
+        //Default/Blog_Single
+        public IActionResult Blog_Single()
+        {
+            return View("Blog_Single");
+        }
+
+
+        //Default/Cart
+        public IActionResult Cart()
+        {
+            return View("Cart");
+        }
+
+        //Default/CheckOut
+        public IActionResult CheckOut()
+        {
+            return View("CheckOut");
+        }
+
+        //Default/Contact_Us
+        public IActionResult Contact_Us()
+        {
+            return View("Contact_Us");
+        }
+
+        //Default/Login
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        //Default/Product_Details
+        public IActionResult Product_Details()
+        {
+            return View("Product_Details");
+        }
+
+        //Default/Error
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+
+
+
     }
 }
